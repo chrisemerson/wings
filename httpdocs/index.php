241dd2ab06c276b689c18cc017bea9e94e2dbad7
Chris Emerson <chris@cemerson.co.uk>
2009-10-08 14:32:51 +0000

    try-catch statement around index.php - catch to be written!

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,6 +1,10 @@
 <?php
  require_once dirname(__FILE__) . "/../app/inc.PROJECTNAME.php";
 
- $objFrontController = new FrontController($_GET['url']);
- $objFrontController->execute();
+ try {
+  $objFrontController = new FrontController($_GET['url']);
+  $objFrontController->execute();
+ } catch (Exception $exException) {
+  print_r($exException->getMessage());
+ }//try
 ?>
