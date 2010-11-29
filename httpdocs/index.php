465d5949b8988cf94b095eb91e2f69dfb6b4bfe6
Chris Emerson <chris@cemerson.co.uk>
2010-11-29 13:42:26 +0000

    Use set_exception_handler rather than try/catch block in index.php

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,10 +1,7 @@
 <?php
   require_once dirname(__FILE__) . "/../app/inc.framework.php";
 
-  try {
-    $objDispatcher = new Dispatcher(isset($_GET['url']) ? $_GET['url'] : '');
-    $objDispatcher->execute();
-  } catch (Exception $exException) {
-    Application::handleUncaughtException($exException);
-  }//try
-?>
+  set_exception_handler(array('Application', 'handleUncaughtException'));
+
+  $objDispatcher = new Dispatcher(isset($_GET['url']) ? $_GET['url'] : '');
+  $objDispatcher->execute();
