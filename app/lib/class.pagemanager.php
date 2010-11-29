b74eda2abc753ca0e180eae30fd191700d294459
Chris Emerson <chris@cemerson.co.uk>
2010-11-29 13:01:21 +0000

    Remove iDate class, cause its rubbish

--- app/lib/class.pagemanager.php
+++ app/lib/class.pagemanager.php
@@ -1,12 +1,4 @@
 <?php
- /*****************************************/
- /* Page Manager Class - by Chris Emerson */
- /* http://www.cemerson.co.uk/            */
- /*                                       */
- /* Version 0.1                           */
- /* 28th May 2009                         */
- /*****************************************/
-
  class PageManager {
 
  }//class
