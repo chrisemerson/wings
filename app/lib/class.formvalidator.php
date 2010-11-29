213a1d0d980a6adc98719c260e1caaad6d6375b5
Chris Emerson <chris@cemerson.co.uk>
2010-11-29 13:00:27 +0000

    Logfile implementation, and form validator shell class

---
+++ app/lib/class.formvalidator.php
@@ -0,0 +1,4 @@
+<?php
+  class FormValidator {
+
+  }//class
--- app/lib/class.logfile.php
+++ app/lib/class.logfile.php
@@ -1,3 +1,36 @@
 <?php
  class LogFile {
+   private $strLogFileName;
+   private $fleLogFile;
+
+   public function __construct ($strLogFileName) {
+     $this->strLogFileName = Application::getBasePath() . "logs/" . $strLogfileName . ".log";
+
+     $this->openLogFile();
+   }//function
+
+   public function __destruct () {
+     $this->closeLogFile();
+   }//function
+
+   public function logMessage ($strMessage) {
+     //Also log time/date & IP address
+     $strDate = date('Y-m-d H:i:s');
+     $strIPAddress = $_SERVER['REMOTE_ADDR'];
+
+     //Convert all new lines, multiple spaces, tabs etc to a single space & trim
+     $strMessage = trim(preg_replace('/\s+/', ' ', $strMessage));
+
+     $strLineToLog = "[" . $strDate . " " . $strIPAddress . "]: " . $strMessage;
+
+     fwrite($this->fleLogFile, $strLineToLog . "\n");
+   }//function
+
+   private function openLogFile () {
+     $this->fleLogFile = fopen($this->strLogFileName, 'a');
+   }//function
+
+   private function closeLogFile () {
+     fclose($this->fleLogFile);
+   }//function
  }//class
