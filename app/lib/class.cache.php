bf25217dbff0b9af3c36bd7e838488881cc6dd80
Chris Emerson <chris@cemerson.co.uk>
2010-12-13 09:32:24 +0000

    Cache library. Stores everything in cache folder as <name>.cache

---
+++ app/lib/class.cache.php
@@ -0,0 +1,29 @@
+<?php
+  class Cache {
+    const CACHE_DIR = 'cache';
+
+    public function save ($mixItem, $strName, $intExpiry = 0) {
+      $strCacheFilename = $this->getCacheFilename($strName);
+
+      $arrCacheContents = array('item' => $mixItem, 'expires' => $intExpiry);
+
+      file_put_contents($strCacheFilename, serialize($arrCacheContents));
+
+    }//function
+
+    public function get ($strName) {
+      $strCacheFilename = $this->getCacheFilename($strName);
+
+      if (file_exists($strCacheFilename)) {
+        $arrCacheContents = unserialize(file_get_contents($strCacheFilename));
+
+        return $arrCacheContents['item'];
+      }//if
+
+      return false;
+    }//function
+
+    private function getCacheFilename ($strCacheName) {
+      return Application::getBasePath() . self::CACHE_DIR . "/" . $strCacheName . ".cache";
+    }//function
+  }//class
