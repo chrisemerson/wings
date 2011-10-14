a1136848e7bf17f77db8716df915277a7a9ffa93
Chris Emerson <chris@cemerson.co.uk>
2011-10-14 16:02:01 +0000

    Change included filename

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,5 +1,5 @@
 <?php
-  require_once dirname(__FILE__) . "/../app/inc.wings.php";
+  require_once dirname(__FILE__) . "/../sys/inc.wings.php";
 
   $objDispatcher = new Dispatcher(isset($_GET['url']) ? $_GET['url'] : '');
   $objDispatcher->execute();
