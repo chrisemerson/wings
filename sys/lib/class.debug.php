142cf502b3e9f37553b6769978c7b107835d50c8
Chris Emerson <chris@cemerson.co.uk>
2011-11-03 15:56:10 +0000

    Debug class documentation

--- sys/lib/class.debug.php
+++ sys/lib/class.debug.php
@@ -18,18 +18,6 @@
       }//if
     }//function
 
-    public static function mt ($blnGetAsFloat) {
-      if (self::isDebugMode()) {
-        microtime($blnGetAsFloat);
-      }//if
-    }//function
-
-    public static function db ($blnProvideObject) {
-      if (self::isDebugMode()) {
-        debug_backtrace($blnProvideObject);
-      }//if
-    }//function
-
     public static function pr ($mixInput, $blnReturn = false) {
       if (self::isDebugMode()) {
         print_r($mixInput, $blnReturn);
