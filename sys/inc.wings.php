8ca4dfb6fbcb9e14c7f8a4f29c130014a6051271
Chris Emerson <chris@cemerson.co.uk>
2012-02-06 22:08:50 +0000

    Remove loadorder stuff and rename files to act as a load order

--- sys/inc.wings.php
+++ sys/inc.wings.php
@@ -1,43 +1,22 @@
 <?php
   //Load all files in the base/ folder
   loadFilesInDirectory(realpath(dirname(__FILE__) . "/base"));
+  loadFilesInDirectory(realpath(dirname(__FILE__) . "/../app/base"));
 
   function loadFilesInDirectory ($strDirectory) {
     $arrFilesInBaseDir = scandir($strDirectory);
 
-    if (file_exists($strDirectory . "/loadorder")) {
-      $arrFilesToLoad = file($strDirectory . "/loadorder");
+    foreach ($arrFilesInBaseDir as $strFilename) {
+      if ($strFilename{0} != '.') {
+        $strFullFilename = $strDirectory . "/" . $strFilename;
 
-      foreach ($arrFilesToLoad as $strLine) {
-        $strFilename = trim($strLine);
-
-        if (substr($strFilename, 0, 1) != "#" && is_file($strDirectory . "/" . trim($strFilename))) {
-          require_once $strDirectory . "/" . trim($strFilename);
+        if (is_file($strFullFilename)) {
+          require_once $strFullFilename;
+        } else if (is_dir($strFullFilename)) {
+          loadFilesInDirectory($strFullFilename);
         }//if
-      }//foreach
-
-      foreach ($arrFilesInBaseDir as $strFilename) {
-        if ($strFilename{0} != '.') {
-          $strFullFilename = $strDirectory . "/" . $strFilename;
-
-          if (is_dir($strFullFilename)) {
-            loadFilesInDirectory($strFullFilename);
-          }//if
-        }//if
-      }//foreach
-    } else {
-      foreach ($arrFilesInBaseDir as $strFilename) {
-        if ($strFilename{0} != '.') {
-          $strFullFilename = $strDirectory . "/" . $strFilename;
-
-          if (is_file($strFullFilename)) {
-            require_once $strFullFilename;
-          } else if (is_dir($strFullFilename)) {
-            loadFilesInDirectory($strFullFilename);
-          }//if
-        }//if
-      }//foreach
-    }//if
+      }//if
+    }//foreach
   }//function
 
   unset($strBasePath);
